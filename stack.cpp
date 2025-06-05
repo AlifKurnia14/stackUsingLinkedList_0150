7ba5d4ce31812fb556bee686a52c01140e069143
AlifKurnia14 <tsimpsons056@gmail.com>
2025-06-05 15:55:49 +0700

    add void peek

--- stack.cpp
+++ stack.cpp
@@ -48,4 +48,12 @@ class Node{
             cout << "Popped value :" << top -> data << endl;
             top = top -> next;
         }
+    
+
+        void peek(){
+            if (isEmpty ()){
+                cout << "List is empty:" << endl;
+                return; // if the stack is empty, print a messege and return
+            }
+        }
     };
